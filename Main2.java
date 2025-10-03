40d84531c1ee6647eb4b9b11c6a0e621ded6e938
Gevorg Vart <109519914+ibmhcr34@users.noreply.github.com>
2025-10-03 10:49:01 +0300

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,46 @@
+public class HeapSort {
+
+    public void heapSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    private void heapify(int[] arr, int n, int i) {
+        int smallest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] < arr[smallest]) smallest = left;
+        if (right < n && arr[right] < arr[smallest]) smallest = right;
+
+        if (smallest != i) {
+            int temp = arr[i];
+            arr[i] = arr[smallest];
+            arr[smallest] = temp;
+
+            heapify(arr, n, smallest);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 10, 3, 5, 1};
+
+        HeapSort sorter = new HeapSort();
+        sorter.heapSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
